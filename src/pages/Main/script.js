6cbd2226ce1910529465447893567a420849d06a
KHJcode <kimstudswcode4485@gmail.com>
2020-10-19 00:57:34 +0900

    page score

--- src/pages/Main/script.js
+++ src/pages/Main/script.js
@@ -2,16 +2,34 @@ const infoLink = document.querySelector('.infoContainer .title a');
 const infoTitle = infoLink.querySelector('h2');
 const infoContent = document.querySelector('.infoContainer .description p');
 const urlBox = document.querySelector('.url_box');
+const truthScore = document.querySelector('.truth');
+const lieScore = document.querySelector('.lie');
 
-const url = 'http://localhost:5050/info/get/news/sample';
+const url1 = 'http://localhost:5050/info/get/news/sample';
+const url2 = 'http://localhost:5050/page/get/score';
 
 const loadInfoData = async (title) => {
-  const res = await fetch(`${url}/${title}/1`);
+  const res = await fetch(`${url1}/${title}/1`);
   const result = await res.json();
 
   infoLink.href = result[0].originallink;
   infoTitle.innerHTML = result[0].title.substring(0, 20) + '...';
   infoContent.innerHTML = result[0].description.substring(0, 100) + '...';
+};
+
+const loadStatusScore = async (url) => {
+  const response = await fetch(url2, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify({ url }),
+  });
+
+  const result = await response.json();
+
+  truthScore.innerHTML = result.truth;
+  falseScore.innerHTML = result.lie;
 };
 
 function init() {
@@ -21,8 +39,14 @@ function init() {
     location.href = '../Login/index.html';
   } else {
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
-      const url = tabs[0].url;
+      let url = tabs[0].url;
       urlBox.value = url;
+
+      if (url.charAt(url.length - 1) === '/') {
+        url = url.substring(0, url.length - 1);
+      }
+      
+      loadStatusScore(url);
     });
 
     chrome.tabs.executeScript({
