8456c03718b1f4b3d0f2afd2fbf63718553e42ef
KHJcode <kimstudswcode4485@gmail.com>
2020-10-23 23:18:15 +0900

    design update

--- src/pages/Check/script.js
+++ src/pages/Check/script.js
@@ -2,6 +2,15 @@ const evalList = document.querySelector('.evalList');
 
 const target = 'https://Truthserver.khjcode.repl.co/page/get/eval';
 
+const rankSystem = {
+  bronze: '170, 125, 98',
+  silver: '186, 181, 178',
+  gold: '164, 147, 104',
+  platinum: '203, 235, 227',
+  diamond: '197, 223, 241',
+  admin: '44, 62, 80',
+};
+
 const loadEvalData = async (url, key) => {
   const data = { url, key };
 
@@ -21,7 +30,7 @@ const drawDOMElement = async (data) => {
   const imgSrc = '../../../public/images/';
 
   data.forEach(item => {
-    let { status, reason, name } = item;
+    let { status, reason, name, rank, createdAt } = item;
 
     let evalBox = document.createElement('div');
     evalBox.classList.add('evalBox');
@@ -53,8 +62,21 @@ const drawDOMElement = async (data) => {
     content.textContent = reason;
     evalContent.appendChild(content);
 
+    let footer = document.createElement('footer');
+    footer.classList.add('footer');
+    let rankName = document.createElement('p');
+    rankName.classList.add('rank');
+    rankName.textContent = rank;
+    rankName.style.background = `rgba(${rankSystem[rank]})`;
+    let date = document.createElement('time');
+    date.classList.add('date');
+    date.textContent = createdAt;
+    footer.appendChild(rankName);
+    footer.appendChild(date);
+
     evalBox.appendChild(header);
     evalBox.appendChild(evalContent);
+    evalBox.appendChild(footer);
 
     evalList.appendChild(evalBox);
   });
