af92e20b5fdcd935345b9e3976670ff4c87dce1c
KHJcode <kimstudswcode4485@gmail.com>
2020-11-09 00:30:53 +0900

    bug fixed

--- src/pages/Info/script.js
+++ src/pages/Info/script.js
@@ -10,42 +10,47 @@ const loadInfoData = async (title) => {
 };
 
 const drawDOMElement = (data) => {
-  data.forEach(item => {
-    let infoBox = document.createElement("div");
-    infoBox.classList.add('infoBox');
-
-    let infoTitleBox = document.createElement("div");
-    infoTitleBox.classList.add('knight_title_div');
-
-    let infoLink = document.createElement("a");
-    let infoTitle = document.createElement("h2");
-    let title = document.createTextNode(item.title);
-    infoTitle.appendChild(title);
-    infoLink.appendChild(infoTitle);
-    infoLink.href = item.originallink;
-    infoLink.target = '_blank';
-
-    let infoContentBox = document.createElement("div");
-    infoContentBox.classList.add('knight_content_div');
-
-    let infoContent = document.createElement("p");
-    let content = document.createTextNode(item.description);
-    infoContent.appendChild(content);
-
-    let hrBox = document.createElement("div");
-    hrBox.classList.add("hr_div");
-    let hr = document.createElement("hr");
-    
-    infoTitleBox.appendChild(infoLink);
-    infoContentBox.appendChild(infoContent);
-    hrBox.appendChild(hr);
-
-    infoBox.appendChild(infoTitleBox);
-    infoBox.appendChild(infoContentBox);
-
-    container.appendChild(infoBox);
-    container.appendChild(hrBox);
-  });
+  if (data[0]) {
+    data.forEach(item => {
+      let infoBox = document.createElement("div");
+      infoBox.classList.add('infoBox');
+  
+      let infoTitleBox = document.createElement("div");
+      infoTitleBox.classList.add('knight_title_div');
+  
+      let infoLink = document.createElement("a");
+      let infoTitle = document.createElement("h2");
+      let title = document.createTextNode(item.title);
+      infoTitle.appendChild(title);
+      infoLink.appendChild(infoTitle);
+      infoLink.href = item.originallink;
+      infoLink.target = '_blank';
+  
+      let infoContentBox = document.createElement("div");
+      infoContentBox.classList.add('knight_content_div');
+  
+      let infoContent = document.createElement("p");
+      let content = document.createTextNode(item.description);
+      infoContent.appendChild(content);
+  
+      let hrBox = document.createElement("div");
+      hrBox.classList.add("hr_div");
+      let hr = document.createElement("hr");
+      
+      infoTitleBox.appendChild(infoLink);
+      infoContentBox.appendChild(infoContent);
+      hrBox.appendChild(hr);
+  
+      infoBox.appendChild(infoTitleBox);
+      infoBox.appendChild(infoContentBox);
+  
+      container.appendChild(infoBox);
+      container.appendChild(hrBox);
+    });
+  } else {
+    alert('관련 정보가 없습니다.');
+    location.href = '../Main/index.html';
+  }
 };
 
 
